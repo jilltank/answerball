bcafaa97afed0b03e97286a35d692c2a7f964032
Jill Tankersley <jillian.emilie@gmail.com>
2016-05-04 11:22:07 -0400

    completed functions to run app. now all the answers to your questions will be revealed.

--- answer_ball/MainVC.swift
+++ answer_ball/MainVC.swift
@@ -9,6 +9,19 @@
 import UIKit
 
 class MainVC: UIViewController {
+    
+    @IBOutlet weak var answerText: UILabel!
+    
+    var answers = [
+        "Yes",
+        "No",
+        "Maybe so",
+        "No idea on that one",
+        "I don't even know why you asked this question",
+        "The force is not with you on this one",
+        "That's not how the force works!",
+    ]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,15 +34,23 @@ class MainVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func motionBegan(motion: UIEventSubtype, withEvent event: UIEvent?) {
+        getAnswerTapped(UIEvent)
     }
-    */
+    
+    @IBAction func getAnswerTapped(sender: AnyObject) {
+        
+        answerText.text = getAnswer()
+        answerText.hidden = false
+        
+    }
+    
+    func getAnswer() -> String {
+        let random = Int(arc4random_uniform(UInt32(answers.count)))
+        let result = answers[random]
+        return result
+    }
+    
 
+    
 }
